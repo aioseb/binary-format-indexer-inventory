971313bc2229cd13d420eb76bd548c986f024609
andrei-sebastian.aionesei <andrei-sebastian.aionesei@info.uaic.ro>
2026-04-15 17:34:18 +0300

    Implemented initial indexer version (TODO: store in binary format)

--- src/main_fileops_indexer.c
+++ src/main_fileops_indexer.c
@@ -6,6 +6,21 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <dirent.h>
+
+#define PATH_MAX 256
+char resolved_path[PATH_MAX + 1];   // Buffer folosit pentru caile absolute in timpul recursiei (pentru a economisi memorie)
+int fd_db = -1;  // File decsriptor-ul global pentru baza de date
+
+struct db_entry{
+    const char* absolute_path;
+    const char* type;
+    off_t size_bytes;
+    time_t mtime;
+    unsigned long long hash;
+    dev_t dev;
+    ino_t inode;
+};
 
 int validate_directory(const char* path){
     if(path == NULL){
@@ -50,6 +65,104 @@ int validate_destination(const char* path){
     return 0;
 }
 
+// Implementare FNV-1a hash pentru 64 biti
+unsigned long long fnv1a(const char* str){
+    unsigned long long hash = 14695981039346656037ULL;
+    const unsigned long long prime = 1099511628211ULL;
+
+    for(int i = 0; str[i]; i++){
+        hash ^= str[i];
+        hash *= prime;
+    }
+
+    return hash;
+}
+
+// Construieste o intrare pentru fisierul respectiv
+int build_entry(const char* path, struct db_entry* entry){
+    struct stat st;
+    if(0 != lstat(path, &st)){
+        perror("Eroare stat entry");
+        return 1;
+    }
+
+    entry->absolute_path = path;
+    entry->size_bytes = st.st_size;
+    entry->mtime = st.st_mtime;
+    entry->hash = fnv1a(path);
+    entry->dev = st.st_dev;
+    entry->inode = st.st_ino;
+
+    // Determina tipul fisierului
+    if(S_ISREG(st.st_mode)) entry->type = "regular";
+    else if(S_ISDIR(st.st_mode)) entry->type = "directory";
+    else if(S_ISLNK(st.st_mode)) entry->type = "symlink";
+    else if(S_ISCHR(st.st_mode)) entry->type = "characterdev";
+    else if(S_ISBLK(st.st_mode)) entry->type = "blockdev";
+    else if(S_ISFIFO(st.st_mode)) entry->type = "fifo";
+    else if(S_ISSOCK(st.st_mode)) entry->type = "socket";
+    else entry->type = "unknown";
+
+    return 0;
+}
+
+// Adauga o intrare in baza de date
+int append_entry(struct db_entry* entry){
+    if(fd_db < 0){
+        fprintf(stderr, "Eroare: baza de date nu este deschisa!\n");
+        exit(2);
+    }
+
+    write(fd_db, entry->absolute_path, sizeof(entry->absolute_path));
+    write(fd_db, " ", 1);
+
+    write(fd_db, entry->type, sizeof(entry->type));
+    write(fd_db, " ", 1);
+
+    write(fd_db, "\n", 1);
+}
+
+// Construieste in maniera recursiva baza de date in <dest>
+int build_database(const char* dir, const char* dest){
+    DIR* dir_stream;
+    struct dirent *ent;
+    struct stat st;
+    struct db_entry entry;
+
+    char* abs_path_dir = realpath(dir, NULL);
+    printf("%s\n", abs_path_dir);
+
+    if(NULL != (dir_stream = opendir(abs_path_dir))){
+        while(NULL != (ent = readdir(dir_stream))){
+            if(strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0){ // Ignora . si ..
+                continue;
+            }
+
+            // Construieste calea absoluta in "resolved_path"
+            snprintf(resolved_path, sizeof(resolved_path), "%s/%s", abs_path_dir, ent->d_name);
+            build_entry(resolved_path, &entry);
+            append_entry(&entry);
+            
+            if(0 != lstat(resolved_path, &st)){
+                perror("Eroare stat build_database");
+                exit(1);
+            };
+            if(S_ISDIR(st.st_mode)){
+                build_database(resolved_path, dest);    // Parcurge recursiv directorul curent
+            }
+        }
+
+        closedir(dir_stream);   
+    }
+    else{
+        perror("Eroare deschidere director");
+        return 3;
+    }
+
+    free(abs_path_dir);
+    return 0;
+}
+
 int main(int argc, char** argv){
     if (argc <= 2){
         fprintf(stderr, "Folosire: %s --root <dir> [--db <path>]\n", argv[0]);
@@ -84,6 +197,14 @@ int main(int argc, char** argv){
         fprintf(stderr, "Eroare: validare destinatie\n");
         exit(1);
     };
+
+    if(-1 == (fd_db = open(dest, O_WRONLY | O_TRUNC))){
+        perror("Eroare deschidere destinatie");
+        exit(1);
+    }
+
+    build_database(dir, dest);
+    close(fd_db);
 
     printf("%s %s\n", dir, dest);
 }
